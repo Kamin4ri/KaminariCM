b2a320eee80cdcc391dd2d04a70bd13f93131894
Kamin4ri <kamin4ridev@gmail.com>
2016-06-03 16:15:49 -0300

    Update build script
    Signed-off-by: Kamin4ri <kamin4ridev@gmail.com>

--- build.sh
+++ build.sh
@@ -3,15 +3,16 @@
 # Variables
 sequence=`seq 1 100`;
 numjobs=0;
-this="KaminariKernel";
+this="KaminariCM";
 
 # Set up the cross-compiler
+export PATH=$HOME/Toolchains/Linaro-5.3-Generic/bin:$PATH;
 export ARCH=arm;
 export SUBARCH=arm;
-export CROSS_COMPILE=$HOME/Toolchains/Linaro-5.2-A7/bin/arm-cortex_a7-linux-gnueabihf-;
+export CROSS_COMPILE=arm-linux-gnueabihf-;
 
 # Output some basic info
-echo -e "Building KaminariKernel...";
+echo -e "Building KaminariKernel (CM13 version)...";
 
 if [ $1 ]; then
 	case $1 in
@@ -38,8 +39,8 @@ if [ $1 ]; then
 			fi;
 			;;
 		*)
-			if [[ `echo $1 | gawk --re-interval "/v/"` != "" ]]; then
-				version=`echo $1 | cut -d"v" -f2`;
+			if [[ `echo $1 | gawk --re-interval "/r/"` != "" ]]; then
+				version=`echo $1 | cut -d"r" -f2`;
 				if [ $2 ]; then
 					case $2 in
 						"clean")
@@ -89,9 +90,9 @@ echo -e "Build started on: `date +"%A, %d %B %Y @ %H:%M:%S %Z (GMT %:z)"`";
 make falcon_defconfig;
 
 if [ $numjobs ] && [ $numjobs != 0 ]; then
-	make -j$numjobs CONFIG_NO_ERROR_ON_MISMATCH=y;
+	make -j$numjobs;
 else
-	make -j4 CONFIG_NO_ERROR_ON_MISMATCH=y;
+	make -j4;
 fi;
 
 # Tell when the build was finished
@@ -104,7 +105,6 @@ builddate_full=`date +"%d %b %Y | %H:%M:%S %Z"`;
 # Make the zip dir if it doesn't exist
 if [ ! -d ../Zip_Cm13 ]; then 
 	mkdir ../Zip_Cm13;
-	cp -rf ../Custom_AnyKernel/* ../Zip_Cm13;
 fi;
 
 # Copy zImage-dtb
@@ -114,9 +114,9 @@ cd ../Zip_Cm13;
 
 # Set zip name
 if [ $version ] && [ "$version" != "" ]; then
-	zipname="KaminariCM13_v"$version"_Falcon";
+	zipname="Kaminari_R"$version"_Falcon";
 else
-	zipname="KaminariCM13_"$builddate"_Falcon";
+	zipname="Kaminari_"$builddate"_Falcon";
 fi;
 
 # Make the zip
